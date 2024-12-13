7482728f2d94c1886f87ca43185d05b84f302506
AdilYassar <adilyassar9898@gmail.com>
2024-12-13 17:38:49 +0500

    tls server and client is completed

--- src/service/TCPProvider.tsx
+++ src/service/TCPProvider.tsx
@@ -6,44 +6,39 @@ import DeviceInfo from "react-native-device-info";
 import { Alert, Platform } from "react-native";
 import { Buffer } from "buffer";
 import RNFS from 'react-native-fs';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import 'react-native-get-random-values';
-import {produce } from 'immer';
-import { recieveFileAck } from "./TCPUtils";
+import { produce } from 'immer';
+import { receiveChunkAck, receiveFileAck, sendChunkAck } from "./TCPUtils";
 
-
-
-
-
-
-interface TCPContextType{
-    server:any;
-    client:any;
-    isConnected:boolean;
-    connectedDevice:any;
-    sentFiles:any[];
-    recievedFiles:any[];
-    totalSentBytes:number;
-    totalRecievedBytes:number;
-    startServer:(port:number)=>void;
-    connectToServer:(host :string,port:number, deviceName:string)=>void;
-    sendMessages:(message:string | Buffer)=>void;
-    sendFileAck:(file:any, type:'file' | 'image')=>void;
-    disconnect:()=>void;
+interface TCPContextType {
+    server: any;
+    client: any;
+    isConnected: boolean;
+    connectedDevice: any;
+    sentFiles: any[];
+    recievedFiles: any[];
+    totalSentBytes: number;
+    totalRecievedBytes: number;
+    startServer: (port: number) => void;
+    connectToServer: (host: string, port: number, deviceName: string) => void;
+    sendMessages: (message: string | Buffer) => void;
+    sendFileAck: (file: any, type: 'file' | 'image') => void;
+    disconnect: () => void;
 }
 
 const TCPContext = createContext<TCPContextType | undefined>(undefined);
 
-export const useTcp = ():TCPContextType=>{
+export const useTcp = (): TCPContextType => {
     const context = useContext(TCPContext);
-    if(!context){
+    if (!context) {
         throw new Error('useTcp must be used within a TCPProvider');
     }
     return context;
 }
 
 const options = {
-    keystore:require('../../tls_certs/server-keystore.p12')
+    keystore: require('../../tls_certs/server-keystore.p12')
 }
 
 export const TCPProvider: FC<{ children: ReactNode }> = ({ children }) => {
@@ -57,48 +52,48 @@ export const TCPProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const [totalSentBytes, setTotalSentBytes] = useState<number>(0);
     const [totalRecievedBytes, setTotalRecievedBytes] = useState<number>(0);
 
-    const {setChunkStore, currentChunkSet, setCurrentChunkSet} = useChunkStore();
+    const { setChunkStore, currentChunkSet, setCurrentChunkSet } = useChunkStore();
 
-    //start server
-const startServer = useCallback((port: number) => {
-    if(server){
-        return
-    }
-    const newServer = TcpSocket.createTLSServer(options, (socket:any)=>{
-        console.log('server connected');
-        setServerSocket(socket);
-        socket.setNoDelay(true);
-        socket.readableHighWaterMark=1024*1024*1;
-        socket.writableHighWaterMark=1024*1024*1;
-        socket.on('data', async(data:any)=>{
-
-            console.log('server data', data.toString());
-           const parsedData = JSON.parse(data.toString());
-              if(parsedData.event === 'connect'){
-                setConnectedDevice(parsedData?.deviceName);
-                setIsConnected(true);
-              }
-
-              if(parsedData.event === 'file_ack'){
-               recieveFileAck(parsedData.file, socket, setRecievedFiles);
-              }
-              
-        })
-        socket.on('close', ()=>{
-            console.log('server closed');
-            setRecievedFiles([]);
-            setTotalRecievedBytes(0);
-            setSentFiles([]);
-            setCurrentChunkSet(null);
-            setChunkStore(null);
-            setIsConnected(false);
-            disconnect();
-
+    const startServer = useCallback((port: number) => {
+        if (server) {
+            return;
         }
-           )
+        const newServer = TcpSocket.createTLSServer(options, (socket: any) => {
+            console.log('server connected');
+            setServerSocket(socket);
+            socket.setNoDelay(true);
+            socket.readableHighWaterMark = 1024 * 1024 * 1;
+            socket.writableHighWaterMark = 1024 * 1024 * 1;
+            socket.on('data', async (data: any) => {
+                console.log('server data', data.toString());
+                const parsedData = JSON.parse(data.toString());
+                if (parsedData.event === 'connect') {
+                    setConnectedDevice(parsedData?.deviceName);
+                    setIsConnected(true);
+                }
+                if (parsedData.event === 'file_ack') {
+                    receiveFileAck(parsedData.file, socket, setRecievedFiles);
+                }
+                if (parsedData.event === 'send_chunk') {
+                    sendChunkAck(parsedData.chunk, socket, parsedData.chunkNo, setTotalRecievedBytes);
+                }
+                if (parsedData.event === 'receive_chunk_ack') {
+                    receiveChunkAck(parsedData.chunk, socket, parsedData.chunkNo, setTotalSentBytes, generateFile);
+                }
+            });
+            socket.on('close', () => {
+                console.log('server closed');
+                setRecievedFiles([]);
+                setTotalRecievedBytes(0);
+                setSentFiles([]);
+                setCurrentChunkSet(null);
+                setChunkStore(null);
+                setIsConnected(false);
+                disconnect();
+            });
             socket.on('error', (error: any) => {
                 console.log('server error', error);
-            })
+            });
         });
         newServer.listen({ port: port, host: '0.0.0.0' }, () => {
             const address = newServer.address();
@@ -107,170 +102,168 @@ const startServer = useCallback((port: number) => {
         newServer.on('error', (error: any) => { console.error('server error', error); });
 
         setServer(newServer);
-}, [server as Socket | TcpSocket.TLSServer | null]);
+    }, [server]);
 
-
-
-const connectToServer = useCallback((host: string, port: number, deviceName: string) => {   
-    if(client){
-        return
-    }
-    const newClient = TcpSocket.connectTLS({ port: port, host: host , 
-        cert:true, 
-        ca:require('../../tls_certs/server-cert.pem')}, () => {
-        setConnectedDevice(deviceName)
-        const myDeviceName = DeviceInfo.getDeviceNameSync();
-        console.log('client connected');
-        newClient.setNoDelay(true);
-        newClient.write(JSON.stringify({deviceName:myDeviceName, event:'connect'}));
-        setIsConnected(true);
-        newClient.readableHighWaterMark=1024*1024*1;
-        newClient.writableHighWaterMark=1024*1024*1;
-    });
-    newClient.on('data', async (data: any) => {
-        const parsedData = JSON.parse(data?.toString());
-        console.log('client data', data.toString());
-        if (data.toString().includes('file')) {
-            const file = JSON.parse(data.toString());
-            setRecievedFiles((prev) => [...prev, file]);
-            setTotalRecievedBytes((prev) => prev + file.size);
+    const connectToServer = useCallback((host: string, port: number, deviceName: string) => {
+        if (client) {
+            return;
         }
-         if(parsedData.event === 'file_ack'){
-               recieveFileAck(parsedData.file, newClient, setRecievedFiles);
-              }
+        const newClient = TcpSocket.connectTLS({ port: port, host: host, cert: true, ca: require('../../tls_certs/server-cert.pem') }, () => {
+            setConnectedDevice(deviceName);
+            const myDeviceName = DeviceInfo.getDeviceNameSync();
+            console.log('client connected');
+            newClient.setNoDelay(true);
+            newClient.write(JSON.stringify({ deviceName: myDeviceName, event: 'connect' }));
+            setIsConnected(true);
+            newClient.readableHighWaterMark = 1024 * 1024 * 1;
+            newClient.writableHighWaterMark = 1024 * 1024 * 1;
+        });
+        newClient.on('data', async (data: any) => {
+            const parsedData = JSON.parse(data?.toString());
+            console.log('client data', data.toString());
+            if (data.toString().includes('file')) {
+                const file = JSON.parse(data.toString());
+                setRecievedFiles((prev) => [...prev, file]);
+                setTotalRecievedBytes((prev) => prev + file.size);
+            }
+            if (parsedData.event === 'file_ack') {
+                receiveFileAck(parsedData.file, newClient, setRecievedFiles);
+            }
+            if (parsedData.event === 'send_chunk') {
+                sendChunkAck(parsedData.chunk, newClient, parsedData.chunkNo, setTotalRecievedBytes);
+            }
+            if (parsedData.event === 'receive_chunk_ack') {
+                receiveChunkAck(parsedData.chunk, newClient, parsedData.chunkNo, setTotalSentBytes, generateFile);
+            }
+        });
+        newClient.on('close', () => {
+            console.log('client closed');
+            setRecievedFiles([]);
+            setTotalRecievedBytes(0);
+            setSentFiles([]);
+            setCurrentChunkSet(null);
+            setChunkStore(null);
+            setIsConnected(false);
+        });
+        newClient.on('error', (error: any) => {
+            console.log('client error', error);
+        });
+        setClient(newClient);
+    }, [client]);
 
-    });
-    newClient.on('close', () => {
-        console.log('client closed');
+    const disconnect = useCallback(() => {
+        if (server) {
+            if (server instanceof TcpSocket.TLSServer) {
+                server.close();
+            }
+            setServer(null);
+        }
+        if (client) {
+            client.destroy();
+            setClient(null);
+        }
         setRecievedFiles([]);
         setTotalRecievedBytes(0);
         setSentFiles([]);
         setCurrentChunkSet(null);
         setChunkStore(null);
         setIsConnected(false);
-    });
-    newClient.on('error', (error: any) => {
-        console.log('client error', error);
-    });
-    setClient(newClient);
+    }, [server, client]);
 
-
-
-
-
-}, [client]);
-
-
-const disconnect = useCallback(() => {  
-    if(server){
-        if (server instanceof TcpSocket.TLSServer) {
-            server.close();
+    const sendMessages = useCallback((message: string | Buffer) => {
+        if (client) {
+            client.write(JSON.stringify({ message }));
+            console.log('message sent from client');
+        } else if (server) {
+            serverSocket.write(JSON.stringify({ message }));
+            console.log('message sent from server');
+        } else {
+            console.error('no client or server connected');
         }
-        setServer(null);
-    }
-    if(client){
-        client.destroy();
-        setClient(null);
-    }
-    setRecievedFiles([]);
-    setTotalRecievedBytes(0);
-    setSentFiles([]);
-    setCurrentChunkSet(null);
-    setChunkStore(null);
-    setIsConnected(false);
-}, [server, client]);
+    }, [client, server]);
 
+    const sendFileAck = async (file: any, type: 'file' | 'image') => {
+        if (currentChunkSet !== null) {
+            Alert.alert('Please wait for the current file to finish sending');
+            return;
+        }
+        const normalizedPath = Platform.OS === 'ios' ? file?.uri?.replace('file://', '') : file?.uri;
+        const fileData = await RNFS.readFile(normalizedPath, 'base64');
+        const buffer = Buffer.from(fileData, 'base64');
+        const CHUNK_SIZE = 1024 * 8;
+        let totalChunks = 0;
+        let chunkArray = [];
+        let offset = 0;
 
-const sendMessages = useCallback((message: string | Buffer) => { 
-    if(client){
-        client.write(JSON.stringify({message}));
-        console.log('message sent from client');
-    }else if(server){
-        serverSocket.write(JSON.stringify({message}));
-        console.log('message sent from server');
-    }else{
-        console.error('no client or server connected');
-    }
+        while (offset < buffer.length) {
+            const chunk = buffer.slice(offset, offset + CHUNK_SIZE);
+            chunkArray.push(chunk);
+            offset += chunk.length;
+        }
 
-}, [client, server]);
+        const rawData = {
+            id: uuidv4(),
+            name: type === 'file' ? file?.name : file?.filename,
+            size: type === 'file' ? file?.size : file?.fileSize,
+            mimeType: type === 'file' ? 'file' : '.jpg',
+            totalChunks
+        };
+        setCurrentChunkSet({ id: rawData.id, totalChunks, chunkArray });
 
+        setSentFiles((prevData: any[]) =>
+            produce(prevData, (draft: any[]) => {
+                draft.push({
+                    ...rawData,
+                    uri: file.uri,
+                });
+            })
+        );
 
-const sendFileAck = async (file: any, type: 'file' | 'image') => {
-    if (currentChunkSet!==null) {
-       Alert.alert('Please wait for the current file to finish sending');
-        return;
-    } 
-    const normalizedPath = Platform.OS === 'ios' ? file?.uri?.replace('file://', '') : file?.uri;
-    const fileData = await RNFS.readFile(normalizedPath, 'base64');
-    const buffer = Buffer.from(fileData, 'base64');
-    const CHUNK_SIZE = 1024 * 8;
-    let totalChunks = 0;
-    let chunkArray = [];
-    let offset = 0;
+        const socket = client || serverSocket;
+        if (!socket) {
+            return;
+        }
+        try {
+            socket.write(JSON.stringify({ event: 'file_ack', file: rawData }));
+            console.log('file data sent ack');
+        } catch (error) {
+            console.error('error sending file data', error);
+        }
+        totalChunks = chunkArray.length;
+    };
 
-    while(offset < buffer.length){
-        const chunk = buffer.slice(offset, offset + CHUNK_SIZE);
-        chunkArray.push(chunk);
-        offset += chunk.length;
-    }
-
-    const rawData = {
-        id: uuidv4(),
-        name: type === 'file' ? file?.name : file?.filename,
-        size:type === 'file' ? file?.size : file?.fileSize,
-        mimeType: type === 'file' ? 'file':'.jpg',
-        totalChunks
-
-    }
-    setCurrentChunkSet({id:rawData.id, totalChunks, chunkArray});
-
-    setSentFiles((prevData: any[]) => 
-        produce(prevData, (draft: any[]) => {
-            draft.push({
-                ...rawData,
-                uri: file.uri,
-            });
-        })
-    );
-
-    const socket = client  || serverSocket ;  
-    if(!socket){
-    return
-    }
-    try{
-        socket.write(JSON.stringify({event:'file_ack',file: rawData}));
-        console.log('file data sent ack');      
-    }catch(error){
-        console.error('error sending file data', error);
-    }
-    totalChunks = chunkArray.length;
-};
-
-return (
-    <TCPContext.Provider value={{
-        server,
-        client,
-        isConnected,
-        connectedDevice,
-        sentFiles,
-        recievedFiles,
-        totalSentBytes,
-        totalRecievedBytes,
-        startServer,
-        connectToServer,
-        disconnect,
-        sendMessages,
-        sendFileAck
-    }}>
-        {children}
-    </TCPContext.Provider>
-);
-
-
-
-
-
-
+    const generateFile = async (chunk: any, chunkNo: number) => {
+        const { chunkStore, resetChunkStore } = useChunkStore.getState();
+        if (!chunkStore) {
+            console.error('no chunk store found');
+            return;
+        }
+        if (chunkStore?.totalChunks !== chunkStore.chunkArray.length) {
+            console.error('chunk array length does not match total chunks');
+        }
+        try {
+            const combinedChunks = Buffer.concat(chunkStore.chunkArray);
+            const platformPath = Platform.OS == 'ios' ? `${RNFS.DownloadDirectoryPath}` : `${RNFS.DocumentDirectoryPath}`;
+            const filePath = `${platformPath}/${chunkStore.name}`;
+            await RNFS.writeFile(filePath, combinedChunks?.toString('base64'), 'base64');
+            setRecievedFiles((prev: any[]) =>
+                produce(prev, (draft: any[]) => {
+                    const fileIndex = draft?.findIndex((f: any) => f.id === chunkStore.id);
+                    if (fileIndex !== -1) {
+                        draft[fileIndex] = {
+                            ...draft[fileIndex],
+                            uri: filePath,
+                            available: true
+                        };
+                    }
+                })
+            );
+            console.log('file saved successfully', filePath);
+            resetChunkStore();
+        } catch (error) {
+            console.log('error combining Chunks or saving file', error);
+        }
+    };
 
     return (
         <TCPContext.Provider value={{
@@ -287,7 +280,6 @@ return (
             disconnect,
             sendMessages,
             sendFileAck
-           
         }}>
             {children}
         </TCPContext.Provider>
--- src/service/TCPUtils.tsx
+++ src/service/TCPUtils.tsx
@@ -1,19 +1,18 @@
-import { Alert } from "react-native"
-import { useChunkStore } from "../database/chunkStore"
-import { current, produce } from "immer"
+import { Alert } from "react-native";
+import { useChunkStore } from "../database/chunkStore";
+import { produce } from "immer";
 
-export const recieveFileAck = async (data: any, socket: any, setRecievedFiles: any) => {
-    const { setChunkStore, chunkStore } = useChunkStore.getState()
+export const receiveFileAck = async (data: any, socket: any, setReceivedFiles: any) => {
+    const { setChunkStore, chunkStore } = useChunkStore.getState();
     if (chunkStore) {
-        Alert.alert('File already exists')
-        return
+        Alert.alert('File already exists');
+        return;
     }
-    setRecievedFiles((prev: any) => {
+    setReceivedFiles((prev: any) => 
         produce(prev, (draft: any) => {
-            draft.push(data)
+            draft.push(data);
         })
-    })
-
+    );
 
     setChunkStore({
         id: data.id,
@@ -22,66 +21,89 @@ export const recieveFileAck = async (data: any, socket: any, setRecievedFiles: a
         size: data?.size,
         mimeType: data?.mimeType,
         chunkArray: [],
-    })
-
+    });
 
     if (!socket) {
-        console.error('socket not found')
-        return
+        console.error('socket not found');
+        return;
     }
     try {
-        await new Promise((resolve) => setTimeout(resolve, 100))
-        console.log('recieved file ack')
-        socket.write(JSON.stringify({ event: 'send_chunk_ack', chunkNo: 0 }))
-        console.log('requested for first  chunk ack')
-
+        await new Promise((resolve) => setTimeout(resolve, 10));
+        const chunkNo = 0; // Initialize chunkNo
+        console.log('requested for next chunk ack', chunkNo + 1);
+        socket.write(JSON.stringify({ event: 'send_chunk_ack', chunkNo: chunkNo + 1 }));
+        console.log('requested for first chunk ack');
     } catch (error) {
         console.error('error sending file data', error);
     }
+};
 
-
-}
-
-
-export const sendChunkAck = async (data: any, socket: any, setTotalSentBytes: any, setSentFiles:any)=>{
-    const { currentChunkSet, resetCurrentChunkSet } = useChunkStore.getState()
-    if(!currentChunkSet){
-        console.error('no current chunk set')
-        return
+export const sendChunkAck = async (data: any, socket: any, setTotalSentBytes: any, setSentFiles: any) => {
+    const { currentChunkSet, resetCurrentChunkSet } = useChunkStore.getState();
+    if (!currentChunkSet) {
+        console.error('no current chunk set');
+        return;
     }
-    if(!socket){
-        console.error('socket not found')
-        return
+    if (!socket) {
+        console.error('socket not found');
+        return;
     }
-    const totalChunks = currentChunkSet.totalChunks
-    try{
-        await new Promise((resolve) => setTimeout(resolve, 100))
-       
-            console.log('sending next chunk')
-            const chunkIndex = data.chunkNo;
-            socket.write(JSON.stringify({event:'send_chunk_ack', chunk: currentChunkSet.chunkArray[chunkIndex].toString('base64'), chunkNo: data.chunkNo+1}))   
-        setTotalSentBytes((prev: number) => prev + currentChunkSet.chunkArray[chunkIndex].length)   
-        if(chunkIndex+2>totalChunks){
-            console.log('all chunk sent')
-            setSentFiles((prevdile: any[]) => 
-                produce(prevdile, (draftFiles: any[]) => {
-                  const fileIndex = draftFiles.findIndex((file) => file.id === currentChunkSet.id);
-                  if(fileIndex !== -1){
-                      draftFiles[fileIndex].available = true;
-
-                  }
+    const totalChunks = currentChunkSet.totalChunks;
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 10));
+        console.log('sending next chunk');
+        const chunkIndex = data.chunkNo;
+        socket.write(JSON.stringify({ event: 'send_chunk_ack', chunk: currentChunkSet.chunkArray[chunkIndex].toString('base64'), chunkNo: data.chunkNo + 1 }));
+        setTotalSentBytes((prev: number) => prev + currentChunkSet.chunkArray[chunkIndex].length);
+        if (chunkIndex + 2 > totalChunks) {
+            console.log('all chunk sent');
+            setSentFiles((prev: any[]) =>
+                produce(prev, (draftFiles: any[]) => {
+                    const fileIndex = draftFiles.findIndex((file) => file.id === currentChunkSet.id);
+                    if (fileIndex !== -1) {
+                        draftFiles[fileIndex].available = true;
+                    }
                 })
             );
-            resetCurrentChunkSet()
+            resetCurrentChunkSet();
         }
-       
-                
-            
-    }catch(error){  
+    } catch (error) {
         console.error('error sending file data', error);
     }
-}
+};
 
-export const recieveChunkAck = async (chunk: any, socket: any, chunkNo: any,setTotalRecievedBytes:any, generateFile: any) => {
-
-}
+export const receiveChunkAck = async (chunk: any, socket: any, chunkNo: any, setTotalReceivedBytes: any, generateFile: any) => {
+    const { chunkStore, resetChunkStore, setChunkStore } = useChunkStore.getState();
+    if (!chunkStore) {
+        console.error('no chunk store found');
+        return;
+    }
+    try {
+        const bufferChunk = Buffer.from(chunk, 'base64');
+        const updatedChunkStore = [...(chunkStore.chunkArray || [])];
+        updatedChunkStore[chunkNo] = bufferChunk;
+        setChunkStore({
+            ...chunkStore,
+            chunkArray: updatedChunkStore
+        });
+        setTotalReceivedBytes((prev: number) => prev + bufferChunk.length);
+    } catch (error) {
+        console.error('error sending file data and updating chunk', error);
+    }
+    if (!socket) {
+        console.error('socket not found');
+        return;
+    }
+    if (chunkNo + 1 === chunkStore?.totalChunks) {
+        console.log('all chunks received');
+        generateFile();
+        resetChunkStore();
+        return;
+    }
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 10));
+        socket.write(JSON.stringify({ event: 'send_chunk_ack', chunkNo: chunkNo + 1 }));
+    } catch (error) {
+        console.error('error sending file data', error);
+    }
+};
